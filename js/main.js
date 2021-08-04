8e9a498e208eac8e5f8ea92e502c681876018f95
Saqib <87003565+saqib-wpbrigade@users.noreply.github.com>
2021-08-04 05:17:49 -0700

    mobile pixel perfect

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
-const nav = document.querySelector('.main-menu')
-const btn = document.querySelector('.hamburger')
+const nav = document.querySelector('.sidebar')
+const btn = document.querySelector('.main-body .toogle-btn')
 
 
 
 btn.addEventListener('click', () => {
-    if (btn.src.match("../img/burger-icon.png")){
-        nav.classList.add('active')
-        btn.src = "../img/menu-close-icon.png"
-        document.body.style.overflow = 'hidden'
-    } else {
-        nav.classList.remove('active')
-        btn.src = "../img/burger-icon.png"
-        document.body.style.overflow = 'hidden scroll'
-    }
+    nav.classList.toggle('active')
+    
 })
